11e8efafffb082059586dd3532b62937da8aa23b
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-06-28 17:51:19 +0600

    Final commit with all functionalities

--- src/Submenu.js
+++ src/Submenu.js
@@ -1,7 +1,48 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
 
 const Submenu = () => {
-  return <h2>submenu component</h2>
-}
+  const {
+    isSubmenuOpen,
+    location,
+    page: { page, links },
+  } = useGlobalContext();
 
-export default Submenu
+  const container = useRef(null);
+  const [columns, setColumns] = useState('col-2');
+
+  useEffect(() => {
+    const { center, bottom } = location;
+    container.current.style.left = `${center}px`;
+    container.current.style.top = `${bottom}px`;
+
+    if (links.length === 3) {
+      setColumns('col-3');
+    }
+    if (links.length >= 4) {
+      setColumns('col-4');
+    }
+  }, [location, links]);
+
+  return (
+    <aside
+      className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+      ref={container}
+    >
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link, index) => {
+          const { url, icon, label } = link;
+          return (
+            <a key={index} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+
+export default Submenu;
